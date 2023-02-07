45d80fb7f97c98c4fc9bb3268d75c60d21ecbd37
Stanislau Kakhnovich <stas_2002@tut.by>
2023-02-07 15:46:29 +0300

    Add "Chapter 4 task 7 unit indicators"

---
+++ Chapter 4 task 7 unit indicator/Chapter 4 task 7 unit indicator/Source.cpp
@@ -0,0 +1,26 @@
+#include "..\..\std_lib_facilities.h"
+int main()
+{
+	double enter_number = 0;
+	double min_number = 0;
+	double max_number = 0;
+	string units = " ";
+	int i = 0;
+	while (true) {
+		cout << "Please enter number.\n";
+		cin >> enter_number >> units;
+		if (i == 0) {
+			min_number = enter_number;
+			max_number = enter_number;
+			++i;
+		}
+		else if (i > 0 && enter_number < min_number) {
+			cout << "The smallest among the introduced " << enter_number << units << '\n';
+			min_number = enter_number;
+		}
+		else if (i > 0 && enter_number > max_number) {
+			cout << "The largest among the introduced " << enter_number << units << '\n';
+			max_number = enter_number;
+		}
+	}
+}
