edf796b6bd812eb1e308e0172a3f373f4a7e8c37
Stanislau Kakhnovich <stas_2002@tut.by>
2023-02-07 17:01:48 +0300

    add "task 9 total output"

--- Chapter 4 task 7 unit indicator/Chapter 4 task 7 unit indicator/Source.cpp
+++ Chapter 4 task 7 unit indicator/Chapter 4 task 7 unit indicator/Source.cpp
@@ -6,25 +6,37 @@ int main()
 	double max_number = 0;
 	string units = " ";
 	int i = 0;
+	double total = 0;
 	while (true) {
-		cout << "Please enter number.\n";
+		cout << "Please enter a number with one of the units: m, in, cm, ft.\n";
 		cin >> enter_number >> units;
+		if (units == "|") break;
 		 if (units == "cm" || units == "m" || units == "in" || units == "ft"){
 			if (i == 0) {
 				min_number = enter_number;
 				max_number = enter_number;
-				++i;
+				
 			}
 			else if (i > 0 && enter_number < min_number) {
-				cout << "The smallest among the introduced " << enter_number << units << '\n';
+				
 				min_number = enter_number;
 			}
 			else if (i > 0 && enter_number > max_number) {
-				cout << "The largest among the introduced " << enter_number << units << '\n';
+				
 				max_number = enter_number;
 			}
+			++i;
+			if (units == "cm") total += enter_number / 100;
+			else if (units == "in") total +=  enter_number * 2.54 / 100;
+			else if (units == "ft") total += enter_number * 12 * 2.54 / 100;
+			else if (units == "m") total +=  enter_number;
 		 }
 		 else cout << "You entered the wrong unit of measure.\n";
 
 	}
+	cout << "The smallest among the introduced " << min_number << 'm' << '\n';
+	cout << "The largest among the introduced " << max_number << 'm' << '\n';
+	cout << "The number of entered values " << i << '\n';
+	cout << "Total amount of entered values: " << total << 'm' << '\n';
+	
 }
