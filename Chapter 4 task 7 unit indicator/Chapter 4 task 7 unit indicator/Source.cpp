b400358efc2f710301b31e55d6c2eb1fc9139043
Stanislau Kakhnovich <stas_2002@tut.by>
2023-02-07 17:14:30 +0300

    add "add vector"

--- Chapter 4 task 7 unit indicator/Chapter 4 task 7 unit indicator/Source.cpp
+++ Chapter 4 task 7 unit indicator/Chapter 4 task 7 unit indicator/Source.cpp
@@ -6,37 +6,39 @@ int main()
 	double max_number = 0;
 	string units = " ";
 	int i = 0;
-	double total = 0;
+	vector<double> distances;
 	while (true) {
 		cout << "Please enter a number with one of the units: m, in, cm, ft.\n";
 		cin >> enter_number >> units;
 		if (units == "|") break;
-		 if (units == "cm" || units == "m" || units == "in" || units == "ft"){
+		if (units == "cm" || units == "m" || units == "in" || units == "ft") {
 			if (i == 0) {
 				min_number = enter_number;
 				max_number = enter_number;
-				
+
 			}
 			else if (i > 0 && enter_number < min_number) {
-				
+
 				min_number = enter_number;
 			}
 			else if (i > 0 && enter_number > max_number) {
-				
+
 				max_number = enter_number;
 			}
 			++i;
-			if (units == "cm") total += enter_number / 100;
-			else if (units == "in") total +=  enter_number * 2.54 / 100;
-			else if (units == "ft") total += enter_number * 12 * 2.54 / 100;
-			else if (units == "m") total +=  enter_number;
-		 }
-		 else cout << "You entered the wrong unit of measure.\n";
+			if (units == "cm") distances.push_back(enter_number / 100);
+			else if (units == "in") distances.push_back(enter_number * 2.54 / 100);
+			else if (units == "ft") distances.push_back(enter_number * 12 * 2.54 / 100);
+			else if (units == "m") distances.push_back(enter_number);
+		}
+		else cout << "You entered the wrong unit of measure.\n";
 
 	}
 	cout << "The smallest among the introduced " << min_number << 'm' << '\n';
 	cout << "The largest among the introduced " << max_number << 'm' << '\n';
 	cout << "The number of entered values " << i << '\n';
-	cout << "Total amount of entered values: " << total << 'm' << '\n';
+	
+	for(double length:distances) cout << length << 'm' << '\n';
+	
 	
 }
