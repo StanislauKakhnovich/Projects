4b1bc62695133d2c9a0ff99668de58e04751b5af
Stanislau Kakhnovich <stas_2002@tut.by>
2023-02-10 13:32:24 +0300

    add function input_recognition

--- Chapter 4 ex 6 Number in word/Chapter 4 ex 6 Number in word/Source.cpp
+++ Chapter 4 ex 6 Number in word/Chapter 4 ex 6 Number in word/Source.cpp
@@ -1,10 +1,9 @@
 #include "..\..\std_lib_facilities.h"
-int main()
+int input_recognition(string digit)
 {
-	cout << "Enter a number from 0 to 9 as a word.\n";
-	vector<string> digit_word = { 
+	vector<string> digit_word = {
 		"zero",
-		"one", 
+		"one",
 		"two",
 		"three",
 		"four",
@@ -13,14 +12,33 @@ int main()
 		"seven",
 		"eigth",
 		"nine"
+
 	};
-	//int digit = 0;
-	//cin >> digit;
-	//cout << "You entered the number " << digit_word[digit] << ".\n";
-	string word = "?";
-	cin >> word;
+	vector<string> digit_digit = {
+		"0",
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9"
+	};
 	for (int i = 0; i < digit_word.size(); ++i) {
-		if (word == digit_word[i]) cout << "You entered the number " << i << ".\n";
+		if (digit == digit_word[i]) return i;
 	}
+	for (int i = 0; i < digit_digit.size(); ++i) {
+		if (digit == digit_digit[i]) return i;
+	}
+}
+
+int main()
+{
+	cout << "Enter a number from 0 to 9 as a word.\n";
+	string digit;
+	cin >> digit;
+	cout << "You entered the number " << input_recognition(digit) << ".\n";
 
 }
