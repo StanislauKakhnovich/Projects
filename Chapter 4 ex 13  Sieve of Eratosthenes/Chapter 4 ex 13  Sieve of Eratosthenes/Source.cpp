ca088c7fb6fa044f1ce6c9623ee38816c0efec64
Stanislau Kakhnovich <stas_2002@tut.by>
2023-02-13 18:41:02 +0300

    correct solution

--- Chapter 4 ex 13  Sieve of Eratosthenes/Chapter 4 ex 13  Sieve of Eratosthenes/Source.cpp
+++ Chapter 4 ex 13  Sieve of Eratosthenes/Chapter 4 ex 13  Sieve of Eratosthenes/Source.cpp
@@ -2,35 +2,23 @@
 
 int main()
 {
-	vector<int> sequence;
-	for (int i = 2; i < 100; ++i) {
-		sequence.push_back(i);
+
+	vector<int> etalon;
+	vector<int> sieve;
+	for (int i = 2; i <= 100; ++i) {
+		etalon.push_back(i);
+		sieve.push_back(i);
 	}
-	int p = sequence[0];
-	int count = 0;
-	//cout << sequence[sequence.size() - 1];
-	
-	while (p * p < sequence[sequence.size()-1]) {
-		++count;
-		cout << p << " " << count << '\n';
-		if (sequence[count] != 0) {
-			for (int i = count; i < sequence.size(); ++i) {
-				if (sequence[i] % p == 0) sequence[i] = 0;
+	int p = etalon[0];
+
+	while (p * p < etalon[etalon.size()-1]) {
+			for (int i = 1; i < etalon.size(); ++i) {
+				if (etalon[i]!=p && etalon[i] % p == 0) sieve[i] = 0;
 			}
-		}
-		
-		
-		
-		p = sequence[count];
-		
-		
+			++p;
 	}
-	//cout << count;
-	//for (int x : sequence) cout << x << '\n';
-	
-	/*
 	vector<int> primes;
-	for (int x : sequence) if (x != 0) primes.push_back(x);
+	for (int x : sieve) if (x != 0) primes.push_back(x);
 	for (int x : primes) cout << x << '\n';
-	*/
+	
 }
