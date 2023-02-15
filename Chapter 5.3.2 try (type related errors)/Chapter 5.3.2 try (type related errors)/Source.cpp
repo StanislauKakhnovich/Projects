44abcd5b0e29f21ab5233cea82c87c218d94de1e
Stanislau Kakhnovich <stas_2002@tut.by>
2023-02-15 13:38:14 +0300

    function fix

--- Chapter 5.3.2 try (type related errors)/Chapter 5.3.2 try (type related errors)/Source.cpp
+++ Chapter 5.3.2 try (type related errors)/Chapter 5.3.2 try (type related errors)/Source.cpp
@@ -1,7 +1,8 @@
 #include "..\..\std_lib_facilities.h"
+int area(int width, int length);
 int main()
 {
-	int area(int width, int length);
+	
 	int x0 = arena(7);
 	int x1 = area(7);
 	int x2 = area("seven", 2);
