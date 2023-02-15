9eec5825d7d57301c0adcd0ae4856f80df38be8a
Stanislau Kakhnovich <stas_2002@tut.by>
2023-02-15 13:24:18 +0300

    first commit

---
+++ Chapter 5.3.2 try (type related errors)/Chapter 5.3.2 try (type related errors)/Source.cpp
@@ -0,0 +1,11 @@
+#include "..\..\std_lib_facilities.h"
+int main()
+{
+	int area(int width, int length);
+	int x0 = arena(7);
+	int x1 = area(7);
+	int x2 = area("seven", 2);
+	x3 = area('x', 2.5);
+	string x4 = area(2, 8);
+
+}
