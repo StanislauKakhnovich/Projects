50f7d28f314381d7abc4def4b0c910230b627dc2
Stanislau Kakhnovich <stas_2002@tut.by>
2023-02-27 16:05:17 +0300

    add ex 3

--- Chapter 5 ex 2 temperature/Chapter 5 ex 2 temperature/Source.cpp
+++ Chapter 5 ex 2 temperature/Chapter 5 ex 2 temperature/Source.cpp
@@ -1,6 +1,10 @@
 #include "..\..\std_lib_facilities.h"
 double ctok(double c)
 {
+	while (c < -273.15) {
+		cout << "You have entered a temperature value below absolute zero. Enter a temperature equal to or higher than -273.15.\n";
+		cin >> c;
+	}
 	double k = c + 273.15;
 	return k;
 }
@@ -10,10 +14,12 @@ int main()
 	cout << "Enter a temperature value in Celsius.\n";
 	double c = 0;
 	cin >> c;
+	/*
 	while (c < -273.15) {
 		cout << "You have entered a temperature value below absolute zero. Enter a temperature equal to or higher than -273.15.\n";
 		cin >> c;
 	}
+	*/
 	double k = ctok(c);
 	cout << "The Celsius temperature you entered is " << k << " Kelvin." << '\n';
 
