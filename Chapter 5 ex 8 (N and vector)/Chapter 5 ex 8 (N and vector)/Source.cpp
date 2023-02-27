7ab55f4b53b433fad5d02e599ad371c553fb3ff8
Stanislau Kakhnovich <stas_2002@tut.by>
2023-02-27 17:48:37 +0300

    first commit

---
+++ Chapter 5 ex 8 (N and vector)/Chapter 5 ex 8 (N and vector)/Source.cpp
@@ -0,0 +1,16 @@
+﻿#include "..\..\std_lib_facilities.h"
+int main()
+{
+	cout << "Enter the number of values to sum:\n";
+	int N = 0;
+	cin >> N;
+	cout << "Enter multiple integers (| to end input).\n";
+	vector<int> numbers;
+	for (int num; cin >> num;) numbers.push_back(num);
+	if (N > numbers.size()) cout << "You are asking to sum more numbers than you entered.\n";
+	int sum = 0;
+	for (int i = 0; i < N; ++i) sum += numbers[i];
+	cout << "The sum of the first " << N << " numbers is: " << sum << '\n';
+
+
+}
