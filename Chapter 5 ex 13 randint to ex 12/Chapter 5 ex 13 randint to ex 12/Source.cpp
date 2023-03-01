1efce1aa9191da6716b2a81dd764e72f4f55e9f9
Stanislau Kakhnovich <stas_2002@tut.by>
2023-03-01 16:10:04 +0300

    first commit

---
+++ Chapter 5 ex 13 randint to ex 12/Chapter 5 ex 13 randint to ex 12/Source.cpp
@@ -0,0 +1,44 @@
+#include "..\..\std_lib_facilities.h"
+int main()
+{
+	cout << "Enter the number.\n";
+	int n = 0;
+	cin >> n;
+	srand(n);
+	vector<int> hidden_number;
+	for (int i = 0; i < 4; ++i) {
+		hidden_number.push_back(randint(9));
+	}
+	for (int x : hidden_number) cout << x << '\n';
+	
+	int bull = 0;
+	while (bull != 4) {
+		bull = 0;
+		cout << "Enter a four - digit number.\n";
+		int num_user = 0;
+		cin >> num_user;
+		int x = 0;
+		int num0 = num_user / 1000;
+		int num1 = num_user % 1000 / 100;
+		vector<int> users = {
+			num_user / 1000,
+			num_user % 1000 / 100,
+			num_user % 100 / 10,
+			num_user % 10
+		};
+		vector<int> control_cows;
+		for (int i = 0; i < 4; ++i) {
+			if (users[i] == hidden_number[i]) ++bull;
+			else control_cows.push_back(hidden_number[i]);
+		}
+		int cow = 0;
+		for (int i = 0; i < control_cows.size(); ++i) {
+			for (int j = 0; j < 4; ++j) {
+				if (control_cows[i] == users[j]) ++cow;
+			}
+		}
+		cout << bull << " bulls " << cow << " cows \n";
+		
+	}
+	
+}
