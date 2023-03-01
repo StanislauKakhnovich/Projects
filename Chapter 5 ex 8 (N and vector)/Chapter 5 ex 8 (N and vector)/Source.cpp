2b28ddfd67da5df7bc0136cab0b7adc6732d4e0d
Stanislau Kakhnovich <stas_2002@tut.by>
2023-03-01 11:42:15 +0300

    add ex 9 (cannot int)

--- Chapter 5 ex 8 (N and vector)/Chapter 5 ex 8 (N and vector)/Source.cpp
+++ Chapter 5 ex 8 (N and vector)/Chapter 5 ex 8 (N and vector)/Source.cpp
@@ -6,7 +6,13 @@ int main()
 	cin >> N;
 	cout << "Enter multiple integers (| to end input).\n";
 	vector<int> numbers;
-	for (int num; cin >> num;) numbers.push_back(num);
+	for (double num; cin >> num;)
+	{
+		int check = num;
+		if (check != num) cout << "The result cannot be represented as an int.\n";
+		numbers.push_back(num);
+	}
+		
 	if (N > numbers.size()) cout << "You are asking to sum more numbers than you entered.\n";
 	int sum = 0;
 	for (int i = 0; i < N; ++i) sum += numbers[i];
