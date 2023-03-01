ccac9b76e57037b175ccb4f07bad9908732e4986
Stanislau Kakhnovich <stas_2002@tut.by>
2023-03-01 15:50:07 +0300

    string to int

--- Chapter 5 ex 12 (bulls and cows)/Chapter 5 ex 12 (bulls and cows)/Source.cpp
+++ Chapter 5 ex 12 (bulls and cows)/Chapter 5 ex 12 (bulls and cows)/Source.cpp
@@ -1,21 +1,23 @@
 #include "..\..\std_lib_facilities.h"
 int main()
 {
-	vector<string> hidden_number = { "1", "4", "3", "2" };
+	vector<int> hidden_number = { 1, 4, 3, 2 };
 	int bull = 0;
 	while (bull != 4) {
 		bull = 0;
 		cout << "Enter a four - digit number.\n";
-		string num_user = "    ";
+		int num_user = 0;
 		cin >> num_user;
-		string x = "";
-		vector<string> users;
-		for (int i = 0; i < 4; ++i) {
-			x = num_user[i];
-			users.push_back(x);
-		}
-
-		vector<string> control_cows;
+		int x = 0;
+		int num0 = num_user / 1000;
+		int num1 = num_user % 1000 / 100;
+		vector<int> users = { 
+			num_user / 1000, 
+			num_user % 1000 / 100,
+			num_user % 100 / 10,
+			num_user % 10
+		};
+		vector<int> control_cows;
 		for (int i = 0; i < 4; ++i) {
 			if (users[i] == hidden_number[i]) ++bull;
 			else control_cows.push_back(hidden_number[i]);
